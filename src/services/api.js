997dfe67bc76fd507f77ae2095a3d2b768dd09dc
Kishan Srivastava <kishansrivastava.01.ks@gmail.com>
2025-04-10 13:18:07 +0530

    changed backend url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 // const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-const API_URL = import.meta.env.VITE_API_URL || "http://69.62.75.39:5000/api";
+// const API_URL = import.meta.env.VITE_API_URL || "http://69.62.75.39:5000/api";
+const API_URL = import.meta.env.VITE_API_URL || "https://api.mendt.in/api";
 
 if (!import.meta.env.VITE_API_URL) {
   console.warn("VITE_API_URL is not set, using default API URL.");
